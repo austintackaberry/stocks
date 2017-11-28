14f0ea0f5d1964b2e29804f71c5aa319c9516657
Austin Tackaberry <austintackaberry@gmail.com>
2017-11-27 20:25:16 -0800

    try again

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if 'ON_HEROKU' in os.environ:
     app = Flask(__name__, static_folder="client/build")
     @app.route('/')
     def index():
-        return send_from_directory('/','index.html')
+        return send_from_directory('client/build','index.html')
 else:
     app = Flask(__name__)
 
