e15e2a52ed395896fab94060c671bdfa5f5fb590
Austin Tackaberry <austintackaberry@gmail.com>
2017-12-09 23:46:06 -0800

    trying to get PWA to work

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -30,7 +30,8 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `client/build/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
