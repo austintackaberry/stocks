2589c8635744f63247d8cc7e68c7007b9dfeae53
Austin Tackaberry <austintackaberry@gmail.com>
2017-12-09 23:32:03 -0800

    trying to get PWA to work

--- client/src/TimeSeriesSparkLineScatterPlot.jsx
+++
@@ -1,7 +0,0 @@
-<TimeSeriesSparkLineScatterPlot
-  data={dailyStockData}
-  height={500}
-  selectX={datum => new Date(datum[0])}
-  selectY={datum => datum[11]}
-  width={500}
-/>
--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -19,7 +19,8 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('ON_HEROKU' in os.environ && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
