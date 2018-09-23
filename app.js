1f6e4be696485435a83c856916ceba8e184bc9e3
austintackaberry <austintackaberry@gmail.com>
2018-09-23 10:53:19 -0700

    Add comments to node server

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ app.get("/getStockdata", async (req, res) => {
   const client = new Client();
   await client.connect();
 
+  // Get random stock id
   const sqlCount = "SELECT COUNT(*) FROM stocks;";
-
   const resCount = await client.query(sqlCount);
   const count = parseInt(resCount.rows[0].count);
   const randId = Math.floor(Math.random() * count);
+
+  // Get data of random stock
   const sqlQuery = "SELECT * FROM stocks WHERE id = " + randId;
   const resResults = await client.query(sqlQuery);
   const formattedResults = resResults.rows[0];
   formattedResults.data = JSON.parse(formattedResults.data);
+
   await client.end();
   res.send(formattedResults);
 });
